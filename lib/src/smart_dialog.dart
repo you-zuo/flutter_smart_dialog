2dc3b4cc9f31235e7ee95fe93988fb4022627f43
you-zuo <wosigehao55@gmail.com>
2021-11-30 11:18:27 +0800

    Update smart_dialog.dart

--- lib/src/smart_dialog.dart
+++ lib/src/smart_dialog.dart
@@ -286,7 +286,7 @@ class SmartDialog {
   }
   static bool removeTag(String tag) {
     var instance = DialogProxy.instance;
-    return instance.dialogMap.keys.remove(tag);
+    return instance.dialogMap.keys.toList.remove(tag);
   }
 }
 
